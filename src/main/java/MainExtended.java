7f1a760b02f75eaf0f21ee58c51a2022f7232598
Emilia Halicka <em.lyszkowska@gmail.com>
2021-09-21 22:42:23 +0200

    Initial version

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Emilia";
+        int myAge = 30;
+        double height = 1.70;
+        char carClass = 'B';
+        boolean sheHasANewCar = true;
+        String domicile = "Gorzów Wielkopolski" ;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(carClass);
+        System.out.println(height);
+        System.out.println(sheHasANewCar);
+        System.out.println(domicile);
+    }
+}
