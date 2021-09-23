1dfd6a7682ac71e2cd641557b6b6f40d1ecd07ef
Emilia Halicka <em.lyszkowska@gmail.com>
2021-09-23 17:40:22 +0200

    Poprawa modulo

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,17 +1,17 @@
 public class LeapYear {
     public static void main(String[] args) {
-        System.out.println("It is a LeapYear!"); //
+        System.out.println(); //
         int tenRok = 2021;
         int kolejnyRok = 2022;
         System.out.println(tenRok);
         System.out.println(kolejnyRok);
         {
-            if (tenRok % 4 && tenRok % 100 && tenRok % 400) {
+            if (tenRok % 4 ==0 && tenRok % 100 ==0 && tenRok % 400 ==0) {
             System.out.println("It is a LeapYear");
             } else {
                 System.out.println("It is not a LeapYear") ;
         }
-         if (tenRok % 4 && tenRok %100) {
+         if (tenRok % 4 ==0 && tenRok % 100 !=0) {
              System.out.println("It is a LeapYear");
          }
         }
