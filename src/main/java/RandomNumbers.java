170acb64dce420a03a1d62f26e7a06cb91d8f8d7
Emilia Halicka <em.lyszkowska@gmail.com>
2021-10-11 20:04:38 +0200

    2.5.2.

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -1,13 +1,35 @@
 public class RandomNumbers {
-    private int[] randomNumbers;
+    private int[] theNumbers;
     int number;
+    int suma = 0;
+    int min;
+    int max;
+    int ilośćLiczb = 31, i;
 
-    public RandomNumbers(int number) {
-        this.randomNumbers = new int[31];
-
-
-
-
+    public int[] RandomNumbers(int number) {
+        int[] theNumbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+        return theNumbers;
 
     }
+
+    public void sumNumber(int number) {
+        int sum = 5000;
+        for (int i = 0; i < theNumbers.length; i++) {
+            sum = sum + number;
+
+            if (sum > 500) {
+                return sum >5000;
+            }
+        }
+    }
+}
+public void minNumber () {
+    min = Math.round(31*(theNumbers.length()));
+    System.out.println("Wylosowano liczby:" + min + ",");
+
+    max= min;
+    suma= suma+max;
+    for (i= 1; i <= theNumbers.length-1; i++);
+
+}
 }
