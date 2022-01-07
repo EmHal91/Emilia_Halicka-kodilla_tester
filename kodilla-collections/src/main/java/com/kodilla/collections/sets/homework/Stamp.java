2f43ba1319baa32e56fa0e04a0e4735300d71fee
Emilia Halicka <em.lyszkowska@gmail.com>
2022-01-07 23:16:46 +0100

    Zadanie 4.4

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -1,6 +1,49 @@
 package com.kodilla.collections.sets.homework;
 
+import java.util.Objects;
+
 public class Stamp {
     private String stampName;
     private double stampDimensions;
+    private String stamp;
+
+    public Stamp (String stampName, double stampDimensions, String stamp){
+        this.stampName = stampName;
+        this.stampDimensions = stampDimensions;
+        this.stamp = stamp;
+    }
+
+    public String getStampName(){
+        return stampName;
+    }
+
+    public double getStampDimensions(){
+        return stampDimensions;
+    }
+
+    public String getStamp (){
+        return stamp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stamp stamp1 = (Stamp) o;
+        return Double.compare(stamp1.stampDimensions, stampDimensions) == 0 && stamp == stamp1.stamp && Objects.equals(stampName, stamp1.stampName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stampName, stampDimensions, stamp);
+    }
+
+    @Override
+    public String toString() {
+        return "Stamp{" +
+                "stampName='" + stampName + '\'' +
+                ", stampDimensions=" + stampDimensions +
+                ", stamp=" + stamp +
+                '}';
+    }
 }
--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -1,7 +1,23 @@
 package com.kodilla.collections.sets.homework;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StampsApplication {
     public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+        stamps.add(new Stamp("Dog", 1.5*2.0, "yes"));
+        stamps.add(new Stamp("Frog", 3.0*2.0, "no"));
+        stamps.add(new Stamp("Car", 2.0*2.0, "no"));
+        stamps.add(new Stamp("Person", 1.5*2.0, "yes"));
+        stamps.add(new Stamp("Mountain", 2.0*2.5, "yes"));
+        stamps.add(new Stamp("Dog", 1.5*2.0, "yes"));
+        stamps.add(new Stamp("Car", 2.0*2.0, "no"));
+
+        System.out.println(stamps.size());
+        for (Stamp stamp : stamps)
+        System.out.println(stamp);
+
 
     }
 }
