aced32e7b95c012a55a0a46bc2918d00cc0cc8a3
Emilia Halicka <em.lyszkowska@gmail.com>
2022-01-17 20:39:20 +0100

    Zad. 5.3.

---
+++ kodilla-collections/kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/TaskHomework.java
@@ -0,0 +1,7 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class TaskHomework {
+    public static void main(String[] args) {
+        Task task = new Task("Potop", 1886);
+    }
+}
